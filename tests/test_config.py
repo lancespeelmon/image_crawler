5dca51e7617b1c55cc0fe5a0a636453059e19c0c
Lance Speelmon <lance@speelmon.com>
2020-04-14 12:41:49 -0700

    docs: document assertions for test_config

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,39 +2,39 @@ import multiprocessing
 from typing import List
 
 from crawler.config import CrawlerConfig
-from logger.logger import get_logger
 from crawler.crawler import Crawler
 from crawler.html_crawler import HtmlCrawler
+from logger.logger import get_logger
 
 LOGGER = get_logger()
 CC = CrawlerConfig(LOGGER)
 
 
 def test_init():
-    assert CC
-    assert CC.logger() is LOGGER
+    assert CC, "CrawlerConfig should not be None"
+    assert CC.logger() is LOGGER, "logger should be same as assigned"
 
 
 def test_concurrency():
     concurrency = CC.concurrency()
-    assert concurrency > 0
-    assert concurrency <= multiprocessing.cpu_count()
+    assert concurrency > 0, "concurrency should be greater than 0"
+    assert concurrency <= multiprocessing.cpu_count(), "concurrency should be less than or equal to number of CPUs"
 
 
 def test_workload():
     workload: List[dict] = CC.workload()
-    assert workload
-    assert len(workload) > 0
+    assert workload, "workload should not be None"
+    assert len(workload) > 0, "workload should have at least one item"
     crawler = workload[0]['crawler']
-    assert crawler
-    assert issubclass(crawler, Crawler)
+    assert crawler, "workload.crawler should not be None"
+    assert issubclass(crawler, Crawler), "crawler should be a subclass of Crawler"
     targets = workload[0]['targets']
-    assert targets
-    assert isinstance(targets, List)
-    assert isinstance(targets[0], str)
+    assert targets, "workload.targets should not be None"
+    assert isinstance(targets, List), "workload.targets should be a List"
+    assert isinstance(targets[0], str), "workload.targets should have at least one String"
 
 
 def test_crawler():
     crawler = CC.crawler(HtmlCrawler)
-    assert crawler
-    assert isinstance(crawler, Crawler)
+    assert crawler, "crawler should not be None"
+    assert isinstance(crawler, Crawler), "crawler should be a Crawler"
